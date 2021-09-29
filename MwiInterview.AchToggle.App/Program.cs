eba652bb3267c110fe1972cd223d6b9b7f9ed27f
Nick Butcher <corsairmarks@hotmail.com>
2021-09-28 21:42:55 -0400

    Initial blank console project + solution

---
+++ MwiInterview.AchToggle.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MwiInterview.AchToggle.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
